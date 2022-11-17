42e343d77b6e5cf59c3b0a13274c97a2566c9fbd
Galla <gallant.thomas@outlook.com>
2022-11-16 22:21:39 -0500

    Added another helpful URL as source.

--- src/app/models/errormodel.ts
+++ src/app/models/errormodel.ts
@@ -1,5 +1,5 @@
 // Code from here: https://dev.to/arealesramirez/how-to-use-error-handler-middleware-with-express-js-and-typescript-431n#:~:text=How%20to%20Write%20a%20Custom%20Error%20Handler%20in,file.%20...%204%204.%20Test%20Custom%20Handler%20
-
+// https://dev.to/qbentil/how-to-write-custom-error-handler-middleware-in-expressjs-using-javascript-29j1
 export class CustomError {
     Error_Message!: string;
     Status!: number;
